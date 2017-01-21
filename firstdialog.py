ae4bd3d911c10e680373a2f9dbbf363270a48790
Utkarsh Raj <clearnote01@gmail.com>
2017-01-21 22:58:12 +0530

    welcome page; main window; exit button

---
+++ firstdialog.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'firstdialog.ui'
+#
+# Created by: PyQt5 UI code generator 5.7
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(310, 185)
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setContentsMargins(9, 9, 9, 9)
+        self.gridLayout.setSpacing(6)
+        self.gridLayout.setObjectName("gridLayout")
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setContentsMargins(9, 9, 9, 9)
+        self.formLayout.setHorizontalSpacing(31)
+        self.formLayout.setVerticalSpacing(5)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.usernameLineEdit = QtWidgets.QLineEdit(Form)
+        self.usernameLineEdit.setObjectName("usernameLineEdit")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.usernameLineEdit)
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.passwordLineEdit = QtWidgets.QLineEdit(Form)
+        self.passwordLineEdit.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.passwordLineEdit.setObjectName("passwordLineEdit")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.passwordLineEdit)
+        self.enterPushButton = QtWidgets.QPushButton(Form)
+        self.enterPushButton.setObjectName("enterPushButton")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.enterPushButton)
+        self.gridLayout.addLayout(self.formLayout, 3, 1, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 3, 2, 1, 1)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem1, 5, 1, 1, 1)
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem2, 3, 0, 1, 1)
+        self.welcomeMessageLabel = QtWidgets.QLabel(Form)
+        self.welcomeMessageLabel.setObjectName("welcomeMessageLabel")
+        self.gridLayout.addWidget(self.welcomeMessageLabel, 0, 1, 1, 1)
+        spacerItem3 = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem3, 1, 1, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "Username:"))
+        self.label_2.setText(_translate("Form", "Password:"))
+        self.enterPushButton.setText(_translate("Form", "Enter"))
+        self.welcomeMessageLabel.setText(_translate("Form", "<html><head/><body><p><span style=\" font-weight:600;\">Welcome to CSMS, have a good day!</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
---
+++ main.py
@@ -0,0 +1,60 @@
+from firstdialog import Ui_Form as welcomeForm
+from MainWindow import Ui_MainWindow as mainWindow
+import sys
+from PyQt5 import QtWidgets
+
+# implement later; return root info from databse
+def getRootInfo():
+    # username, password
+    return "ut","12"
+
+# first dialog box; username password verify`
+class CSMSWelcome(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = welcomeForm()
+        self.ui.setupUi(self)
+        self.setWindowTitle('CSMS Software')
+        self.ui.enterPushButton.clicked.connect(self.verify)
+
+    def verify(self):
+        usernameInput = self.ui.usernameLineEdit.text()
+        passwordInput = self.ui.passwordLineEdit.text()
+        print(usernameInput,passwordInput)
+        if (usernameInput,passwordInput)==getRootInfo():
+            text = "Congrats! You can come in"
+            # implement root user coming code
+            self.close()
+            self.initialiseMain()
+        else:
+            text = "Wrong username or password! Try again"
+        text = "<b>"+text+"</b"
+        self.ui.welcomeMessageLabel.setText(text)
+
+    def initialiseMain(self):
+        csms2.show()
+
+# main application window
+class CSMSMain(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = mainWindow()
+        self.ui.setupUi(self)
+        # A method for exitting application
+        self.ui.menuExit.mouseReleaseEvent = self.exitsafely
+
+    def exitsafely(self,*args):
+        self.close()
+
+if __name__=='__main__':
+    # initialise the application
+    app = QtWidgets.QApplication(sys.argv)
+    # login screen
+    csmsLogin = CSMSWelcome()
+    csmsLogin.show()
+    # main window
+    csms2 = CSMSMain()
+    sys.exit(app.exec_())
+
+
+

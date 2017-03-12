b73fae03c74a2e75a67aef556207557b83d9421e
Utkarsh Raj <clearnote01@gmail.com>
2017-03-12 12:03:17 +0530

    Additional gui for diff account types

--- Pages/createAccount.py
+++ Pages/createAccount.py
@@ -28,22 +28,40 @@ class CreateAccountForm(Ui_Form,QtWidgets.QWidget):
             self.accountTypeComboBox.addItem(accountTypes[item])
 
         self.accountType = self.accountTypeComboBox.currentText()
-        self.createFieldGui(self.accountType)
+        # Trigger action for account Type
+        self.accountTypeComboBox.activated.connect(self.createFieldGui)
 
-    def createFieldGui(self, accountType):
+    def createFieldGui(self, accountTypeIndex):
         # Fix account type
-        if accountType == accountTypes[0]:
-            print(accountType)
+        if accountTypeIndex == 0:
+            print(accountTypes[accountTypeIndex])
             self.termLineEdit.hide()
-        # Sav account type
-        if accountType == accountTypes[1]:
-            print(accountType)
-            self.termLineEdit.hide()
+            self.termLabel.hide()
             self.durationLineEdit.hide()
+            # Duration label
+            self.label_8.hide()
+            self.amountLineEdit.show()
+            self.amountLabel.show()
+        # Sav account type
+        if accountTypeIndex == 1:
+            print(accountTypes[accountTypeIndex])
+            self.termLineEdit.hide()
+            self.termLabel.hide()
+            self.durationLineEdit.hide()
+            # Duration label
+            self.label_8.hide()
             self.amountLineEdit.hide()
+            self.amountLabel.hide()
         # Rec account type
-        if accountType == accountTypes[2]:
-            print(accountType)
+        if accountTypeIndex == 2:
+            self.termLineEdit.show()
+            self.termLabel.show()
+            self.durationLineEdit.show()
+            # Duration label
+            self.label_8.show()
+            self.amountLineEdit.show()
+            self.amountLabel.show()
+            print(accountTypes[accountTypeIndex])
     
     def createAccount(self):
         # memberId=self.memberIdLineEdit.text()
@@ -51,22 +69,23 @@ class CreateAccountForm(Ui_Form,QtWidgets.QWidget):
         accountType=self.accountTypeComboBox.currentText()
         accountNo = self.accountNoLineEdit.text()
         date=self.openningDateEdit.text()
-        self.accountType.get(accountType)(accountNo, date)
-        print(accountType, accountNo, date)
+        rate = self.rateLineEdit.text()
+        self.accountType.get(accountType)(accountNo, date, rate)
+        print(accountType, accountNo, date, rate)
 
-    def createFixedAccount(self, accountNo, date):
+    def createFixedAccount(self, accountNo, date, rate):
         from Models.fixedDeposit import FixedDeposit
         fd = FixedDeposit(accountNo, date)
         db.session.add(fd)
         db.session.commit()
 
-    def createSavingAccount(self, accountNo, date):
+    def createSavingAccount(self, accountNo, date, rate):
         from Models.saving import Saving 
         sa = Saving(accountNo, date)
         db.session.add(sa)
         db.session.commit()
 
-    def createRecurringAccount(self, accountNo, date):
+    def createRecurringAccount(self, accountNo, date, rate):
         from Models.recurring import Recurring 
         ra = Recurring(accountNo, date)
         db.session.add(ra)
